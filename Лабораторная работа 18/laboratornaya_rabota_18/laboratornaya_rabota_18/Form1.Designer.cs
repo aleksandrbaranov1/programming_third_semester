bb2f659bd667cc4598cc1da005205e4cae07b26f
а <sabbarik5@gmail.com>
2024-11-30 04:44:02 +0300

    Добавлена 18

---
+++ Лабораторная работа 18/laboratornaya_rabota_18/laboratornaya_rabota_18/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace laboratornaya_rabota_18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.preview = new System.Windows.Forms.Button();
+            this.exportIntoExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1366, 272);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // preview
+            // 
+            this.preview.Location = new System.Drawing.Point(403, 332);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(163, 23);
+            this.preview.TabIndex = 1;
+            this.preview.Text = "Предпросмотр";
+            this.preview.UseVisualStyleBackColor = true;
+            this.preview.Click += new System.EventHandler(this.preview_Click);
+            // 
+            // exportIntoExcel
+            // 
+            this.exportIntoExcel.Location = new System.Drawing.Point(818, 332);
+            this.exportIntoExcel.Name = "exportIntoExcel";
+            this.exportIntoExcel.Size = new System.Drawing.Size(163, 23);
+            this.exportIntoExcel.TabIndex = 2;
+            this.exportIntoExcel.Text = "Выгрузка в Excel";
+            this.exportIntoExcel.UseVisualStyleBackColor = true;
+            this.exportIntoExcel.Click += new System.EventHandler(this.exportIntoExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1390, 388);
+            this.Controls.Add(this.exportIntoExcel);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Задание №18 выполнил: Баранов А.А., номер варианта: 7, дата выполнения: 30.11.202" +
+    "4";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button preview;
+        private System.Windows.Forms.Button exportIntoExcel;
+    }
+}
+
---
+++ Лабораторная работа 18/laboratornaya_rabota_18/laboratornaya_rabota_18/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelHelpers;
+
+namespace laboratornaya_rabota_18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void preview_Click(object sender, EventArgs e)
+        {
+            string[] headers = "класс предмет;вид работы;1;2;3;4;5;6;7;8;9;10;11".Split(new char[] { ';' });
+            string dataCsvPath = @"C:\Users\sabba\OneDrive\Рабочий стол\programming_third_semester\programming_third_semester\laboratornaya_rabota_18\laboratornaya_rabota_18\bin\Debug\dataCsv.csv.txt";
+            foreach (string header in headers)
+            {
+                dataGridView1.Columns.Add(header, header);
+            }
+
+            StreamReader reader = new StreamReader(dataCsvPath);
+            for (int i = 0; i < 6; i++)
+            {
+                string line = reader.ReadLine();
+                string[] row = line.Split(new char[] { ';' });
+                dataGridView1.Rows.Add(row);
+            }
+            reader.Close();
+        }
+
+        private void exportIntoExcel_Click(object sender, EventArgs e)
+        {
+            string[] headers = "класс предмет;вид работы;1;2;3;4;5;6;7;8;9;10;11".Split(new char[] { ';' });
+            string dataCsvPath = @"C:\Users\sabba\OneDrive\Рабочий стол\programming_third_semester\programming_third_semester\laboratornaya_rabota_18\laboratornaya_rabota_18\bin\Debug\dataCsv.csv.txt";
+            Excel.Application app = new Excel.Application();
+            app.Visible = false;
+            Excel.Workbook wb = app.Workbooks.Add(Missing.Value);
+            Excel.Worksheet ws = (Excel.Worksheet)wb.Sheets.Add();
+            ws.Activate();
+
+            for (int i = 0; i < 13; i++)
+            {
+                ws.Cells[1, i + 1] = headers[i];
+                ws.Cells[1, i + 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                ws.Cells[1, i + 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            }
+
+            StreamReader reader = new StreamReader(dataCsvPath);
+            for (int i = 0; i < 6; i++)
+            {
+                string line = reader.ReadLine();
+                string[] row = line.Split(new char[] { ';' });
+                for (int j = 0; j < row.Length; j++)
+                {
+                    ws.Cells[i + 2, j + 1] = row[j];
+                    ws.Cells[i + 2, j + 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    ws.Cells[i + 2, j + 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                }
+            }
+            reader.Close();
+           
+            ws.Cells[1, 1] = "класс" + "\n" + "предмет";
+            ws.Cells[6, 2] = "контрол." + "\n" + "раб.";
+            ws.Cells[1, 1].Borders[Excel.XlBordersIndex.xlDiagonalDown].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.Range[ExcelMethods.ExcelCellTranslator(2, 1), ExcelMethods.ExcelCellTranslator(6, 1)].Merge();
+            ws.Range[ExcelMethods.ExcelCellTranslator(2, 1), ExcelMethods.ExcelCellTranslator(6, 1)].VerticalAlignment = Excel.XlVAlign.xlVAlignTop;
+            ws.Columns[1].ColumnWidth = 30;
+            ws.Columns[2].ColumnWidth = 20;
+            ws.Range[ExcelMethods.ExcelCellTranslator(1, 1), ExcelMethods.ExcelCellTranslator(7, 13)].Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            app.UserControl = true;
+            wb.SaveCopyAs(Application.StartupPath + Name + ".xlsx");
+            wb.Close(false);
+
+        }
+    }
+}
+
